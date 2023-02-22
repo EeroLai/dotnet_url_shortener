2d1bfa234af14918279df8ad8b8fd95c7b08efda
EeroLai <84887419+EeroLai@users.noreply.github.com>
2023-02-22 20:45:06 +0800

    Add ShortUrl Api

---
+++ dotnet_url_shortener/Controllers/API/ShortUrlController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_url_shortener.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShortUrlController : ControllerBase
+    {
+    }
+}
