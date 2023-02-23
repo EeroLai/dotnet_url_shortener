e224f600c2ac65dd10ec6068fb117862619c91f6
EeroLai <84887419+EeroLai@users.noreply.github.com>
2023-02-23 22:18:10 +0800

    Add new html and Update csharp

--- dotnet_url_shortener/Controllers/HomeController.cs
+++ dotnet_url_shortener/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace dotnet_url_shortener.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Dashboard()
         {
             return View();
         }
